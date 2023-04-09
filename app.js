aebebb30bf29126401e86f47a883bd54d545c23f
sanskar12k <sanskarmodanwal8@gmail.com>
2023-04-10 01:07:58 +0530

    Set header for Access control origin

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const user = require('./routes/user');
 const sale = require('./routes/sale')
 const {sms} = require('./mailer/sms');
   app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', 'https://akt-frontend.vercel.app');
     res.header('Access-Control-Allow-Credentials', true);
     res.header(
         'Access-Control-Allow-Headers',
